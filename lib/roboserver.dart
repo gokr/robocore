24796a27f576b939e58f1aad2b7a2b56040c08f4
Göran Krampe <goran.krampe@gmail.com>
2020-10-16 11:41:06 +0200

    Cleaner roboserver

--- lib/roboserver.dart
+++ lib/roboserver.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:cron/cron.dart';
 import 'package:logging/logging.dart';
 import 'package:robocore/core.dart';
 import 'package:robocore/model/swap.dart';
@@ -21,76 +20,7 @@ class Roboserver {
   /// Subscription for Ethereum events
   late StreamSubscription subscription;
 
-  // Keeping track of some state, queried every minute
-  late num rewardsInCORE, rewardsInUSD;
-
-  num lastPriceCOREinUSD = 0;
-
-  late num priceETHinUSD,
-      priceETHinCORE,
-      priceCOREinETH,
-      priceCOREinUSD,
-      poolCORE,
-      poolETH,
-      poolK,
-      poolETHinUSD,
-      poolCOREinUSD,
-      priceLPinUSD,
-      priceLPinETH,
-      floorCOREinUSD,
-      floorCOREinETH,
-      floorLPinUSD,
-      floorLPinETH,
-      floorLiquidity,
-      supplyLP;
-
   Roboserver(this.config);
-
-  /// Run contract queries
-  query() async {
-    await updatePriceInfo();
-    rewardsInCORE = raw18(await core.cumulativeRewardsSinceStart());
-    rewardsInUSD = rewardsInCORE * priceCOREinUSD;
-  }
-
-  /// Call getReserves on both CORE-ETH and ETH-USDT pairs on Uniswap
-  /// and update calculations of prices and pooled tokens.
-  updatePriceInfo() async {
-    var reserves = await core.getReservesCORE2ETH();
-    poolCORE = raw18(reserves[0]);
-    poolETH = raw18(reserves[1]);
-    poolK = poolCORE * poolETH;
-    reserves = await core.getReservesETH2USDT();
-    // Base is ETH price in USD
-    priceETHinUSD = raw6(reserves[1]) / raw18(reserves[0]);
-    priceETHinCORE = poolCORE / poolETH;
-    // Price of CORE
-    priceCOREinETH = poolETH / poolCORE;
-    priceCOREinUSD = priceCOREinETH * priceETHinUSD;
-    // Pool values
-    poolCOREinUSD = poolCORE * priceCOREinUSD;
-    poolETHinUSD = poolETH * priceETHinUSD;
-
-    // This is all LPs minted so far
-    supplyLP = raw18(await core.totalSupplyCORE2ETH());
-    // Price of LP is calculated as the full pool valuated in ETH, divided by supply
-    priceLPinETH = ((poolCORE * priceCOREinETH) + poolETH) / supplyLP;
-    priceLPinUSD = priceLPinETH * priceETHinUSD;
-
-    // Floor calculations
-    // Then k needs to be preserved so if we sell all outsideCORE into the pool
-    // then new poolETH needs to be this (all 10000 CORE now in pool) in order
-    // to make sure poolK stays the same.
-    var newPoolETH = poolK / 10000;
-    // This then gives us a new price - the so called floor price
-    floorCOREinETH = newPoolETH / 10000;
-    floorCOREinUSD = floorCOREinETH * priceETHinUSD;
-    // The liquidity is simply twice newPoolETH
-    floorLiquidity = newPoolETH * 2;
-    // And then we can also calculate floor of LP
-    floorLPinETH = floorLiquidity / supplyLP;
-    floorLPinUSD = floorLPinETH * priceETHinUSD;
-  }
 
   start() async {
     await openDatabase(config);
@@ -110,23 +40,11 @@ class Roboserver {
     core = Core.randomKey(config['apiurl'], config['wsurl']);
     await core.readContracts();
 
-    // Run cron
-    var cron = Cron();
-    // One initial query
-    await query();
-    log.info("Scheduling CORE queries");
-    cron.schedule(new Schedule.parse("*/1 * * * *"), () async {
-      log.info('Running queries ...');
-      await query();
-      log.info('Done queries.');
-    });
-
     // We listen to all Swaps on COREETH
     subscription = core.listenToEvent(core.CORE2ETH, 'Swap', (ev, event) {
       //print("Topics: ${event.topics} data: ${event.data}");
       var swap = Swap.from(ev, event);
       swap.save();
-      updatePriceInfo();
     });
   }
 }
