742404813f997d7c33bcd68c4a713705d2300408
Göran Krampe <goran.krampe@gmail.com>
2020-10-19 15:42:46 +0200

    One more heart :)

--- lib/util.dart
+++ lib/util.dart
@@ -1,6 +1,6 @@
 String makeRepeatedString(int n, String str) {
   var buf = StringBuffer();
-  for (int i = 1; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     buf.write(str);
   }
   return buf.toString();
