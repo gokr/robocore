8822e14995a2a6184ac25b3decf996653be697ba
Göran Krampe <goran.krampe@gmail.com>
2020-10-20 23:50:18 +0200

    Added Etherscan  API

---
+++ lib/etherscan.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class Etherscan {
+  late Client httpClient;
+
+  static const String host = "api.etherscan.io";
+  String apiKey;
+
+  Etherscan(this.apiKey) {
+    httpClient = Client();
+  }
+
+  Future<Map> _transaction(String txn, String action, String module) async {
+    var params = {
+      "module": module,
+      "action": action,
+      "txhash": txn,
+      "apikey": apiKey
+    };
+    var uri = Uri.https(host, '/api', params);
+    var response = await httpClient.get(uri);
+    return jsonDecode(response.body);
+  }
+
+  Future<Map> getstatus(String txn) async {
+    return _transaction(txn, "getstatus", "transaction");
+  }
+
+  Future<Map> txlistinternal(String txn) async {
+    return _transaction(txn, "txlistinternal", "account");
+  }
+
+  Future<Map> ethGetTransactionByHash(String txn) async {
+    return _transaction(txn, "eth_getTransactionByHash", "proxy");
+  }
+}
