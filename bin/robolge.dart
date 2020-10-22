efc5a0f310127bc9eed016e819c0e63e37411e00
Göran Krampe <goran.krampe@gmail.com>
2020-10-22 18:28:30 +0200

    Cleanup

--- bin/robolge.dart
+++ bin/robolge.dart
@@ -7,11 +7,7 @@ main(List<String> arguments) async {
   Map config = loadConfig();
   var robo = RoboLGE(config);
   await robo.start();
-  //if (arguments[0] == "recreate") {
   await robo.recreateHistory();
   await robo.checkContract();
-  //} else if (arguments[0] == "check") {
-  //  robo.checkContract();
-  //}
   exit(0);
 }
