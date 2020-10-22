dc87c741bf1e7324db6641658901a75863bb95bd
Göran Krampe <goran.krampe@gmail.com>
2020-10-22 02:58:08 +0200

    First scan calculating units almost working

--- lib/robolge.dart
+++ lib/robolge.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:logging/logging.dart';
 import 'package:robocore/core.dart';
@@ -49,7 +50,8 @@ class RoboLGE {
     scan = Etherscan(config['etherscan']);
 
     // Uniswap
-    uniswap = Uniswap(config['uniswap']);
+    uniswap = Uniswap(); //config['uniswap']);
+    await uniswap.connect();
 
     // Create our interface with Ethereum
     core = Core.randomKey(config['apiurl'], config['wsurl']);
@@ -103,9 +105,23 @@ class RoboLGE {
     } else if (adr == core.wbtcAddr) {
       return core.wbtcAddr;
     } else if (adr == core.WBTC2ETHAddr) {
-      return core.wbtcAddr;
+      return core.WBTC2ETHAddr;
     } else {
       throw "Unknown COIN deposit";
+    }
+  }
+
+  String symbolOfDepositToken(EthereumAddress adr) {
+    if (adr == core.coreAddr) {
+      return 'CORE';
+    } else if (adr == core.wethAddr) {
+      return 'WETH';
+    } else if (adr == core.wbtcAddr) {
+      return 'WBTC';
+    } else if (adr == core.WBTC2ETHAddr) {
+      return 'WBTC/ETH LP';
+    } else {
+      throw "Unknown COIN";
     }
   }
 
@@ -115,11 +131,11 @@ class RoboLGE {
     } else if (adr == core.WBTC2ETHAddr) {
       return 'WBTC';
     } else {
-      throw "Unknown COIN deposit";
+      throw "Unknown COIN swap";
     }
   }
 
-  String symbolOfToken(EthereumAddress adr) {
+  String symbolOfTokenTransfer(EthereumAddress adr) {
     if (adr == core.coreAddr) {
       return 'CORE';
     } else if (adr == core.wethAddr) {
@@ -133,6 +149,13 @@ class RoboLGE {
 
   processTransfers(
       TransactionInformation tx, List<FilterEvent> logs, Contribution c) {}
+
+  Future<BigInt> coreValueOfWBTC(BigInt amount, BlockNum block) async {
+    var priceCOREinETH = await uniswap.pairPriceAt(block, core.CORE2ETHAddr);
+    var priceWBTCinETH = await uniswap.pairPriceAt(block, core.WBTC2ETHAddr);
+    return BigInt.from(
+        pow(10, 18) * ((raw8(amount) * priceWBTCinETH) / priceCOREinETH));
+  }
 
   recreateHistory() async {
     var contribs = await Contribution.getAll();
@@ -149,7 +172,7 @@ class RoboLGE {
       // If holder is not mapped yet, we do it
       if (contrib.holder == null) {
         contrib.holder = holder.id;
-        contrib.update();
+        await contrib.update();
       }
       // Find tx info
       var ethUrl = "https://etherscan.io/tx/${contrib.tx}";
@@ -176,13 +199,16 @@ class RoboLGE {
         print("BAD APPLE! $ethUrl");
         bad.add(tx);
       }
+      if (contrib.units > BigInt.parse("65487333214482957510")) {
+        print("break");
+      }
       if (cos.length == 1) {
         var co = cos.first;
         var cv = hexToInt(co.data.substring(2, 66));
         if (contrib.coreValue != cv) {
           print("Ok, mending coreValue");
           contrib.coreValue = cv;
-          contrib.update();
+          await contrib.update();
           mended.add(tx);
         }
       } else {
@@ -209,11 +235,34 @@ class RoboLGE {
           // Should only be one with this function!
           assert(toLGE.length == 1);
           var trans = toLGE.first;
-          // Pick out amount and token
+          // Pick out amount and token from Transfer
           var amount = hexToInt(trans.data);
           var token = tokenOfSwapTransfer(trans);
-          print("Transfer amount: ${raw18(amount)},  ${symbolOfSwap(token)}");
-          // TODO: Lookup price and calculate proper units here!
+          if (token == core.CORE2ETHAddr) {
+            // ETH was deposited and caused a buy of CORE
+            var priceCOREinETH =
+                await uniswap.pairPriceAt(tx.blockNumber, token);
+            print("Price: $priceCOREinETH ETH/CORE");
+            var coreHistoric = tx.value.getInEther.toDouble() / priceCOREinETH;
+            print("Historic value in CORE: $coreHistoric");
+            print("Transfer amount: ${raw18(amount)},  ${symbolOfSwap(token)}");
+            print("Using amount");
+            contrib.units = amount;
+          } else if (token == core.WBTC2ETHAddr) {
+            // ETH was deposited and caused a buy of WBTC
+            var priceCOREinETH =
+                await uniswap.pairPriceAt(tx.blockNumber, token);
+            print("Price: $priceCOREinETH ETH/CORE");
+            var coreHistoric = tx.value.getInEther.toDouble() / priceCOREinETH;
+            print("Historic value of ETH in CORE: $coreHistoric");
+            var coreHistoric2 = await coreValueOfWBTC(amount, tx.blockNumber);
+            print("Historic value of WBTC in CORE: ${raw18(coreHistoric2)}");
+            print("Transfer amount: ${raw8(amount)},  ${symbolOfSwap(token)}");
+            print("Using historic value of WBTC in CORE");
+            contrib.units = coreHistoric2;
+          }
+          contrib.coin = 'ETH';
+          await contrib.update();
           break;
         case addLiquidityWithTokenWithAllowance:
           print(
@@ -221,6 +270,7 @@ class RoboLGE {
           // Token param
           var token = EthereumAddress.fromHex(
               hexToInt(bytesToHex(tx.input.sublist(5, 36))).toRadixString(16));
+          contrib.coin = symbolOfDepositToken(token);
           tokens.add(tx);
           // All transfers
           var transfers = findTransferEvents(logs);
@@ -230,18 +280,40 @@ class RoboLGE {
                   element.topics.last ==
                   '0x000000000000000000000000f7ca8f55c54cbb6d0965bc6d65c43adc500bc591')
               .toList();
+          var units = BigInt.zero;
           for (var trans in toLGE) {
             var token = tokenOfTokenTransfer(trans);
             // We don't care for WBTC-ETH LPs, those are broken up, burned and cause more transfers
             if (token != core.WBTC2ETHAddr) {
               var amount = hexToInt(trans.data);
-              print(
-                  "Transfer amount: ${raw18(amount)},  ${symbolOfToken(token)}");
+              if (token == core.coreAddr) {
+                // CORE was deposited, we take it as it is
+                print(
+                    "Transfer amount: ${raw18(amount)},  ${symbolOfTokenTransfer(token)}");
+                print("Using amount");
+                units += amount;
+              } else if (token == core.wethAddr) {
+                print(
+                    "Ignoring transfer amount: ${raw18(amount)},  ${symbolOfTokenTransfer(token)}");
+              } else if (token == core.wbtcAddr) {
+                // WBTC was deposited, we value it via WBTC->ETH->CORE
+                var coreHistoric =
+                    await coreValueOfWBTC(amount, tx.blockNumber);
+                print("Historic value of WBTC in CORE: ${raw18(coreHistoric)}");
+                print(
+                    "Transfer amount: ${raw8(amount)},  ${symbolOfTokenTransfer(token)}");
+                print("Using historic value");
+                units += coreHistoric;
+              } else {
+                print("Ehum");
+              }
             } else {
-              print("... Breaking up WBTC-ETH LP");
+              print("Ignoring breaking up WBTC-ETH LP");
               lps.add(tx);
             }
           }
+          contrib.units = units;
+          await contrib.update();
           break;
         case addLiquidityAtomic:
           print("addLiquidityAtomic: ${tx.value}");
--- lib/uniswap.dart
+++ lib/uniswap.dart
@@ -1,17 +1,19 @@
 import 'package:graphql/client.dart';
+import 'package:web3dart/credentials.dart';
+import 'package:web3dart/web3dart.dart';
 
 class Uniswap {
   late GraphQLClient client;
 
-  static const String url =
-      "https://thegraph.com/explorer/subgraph/uniswap/uniswap-v2";
-  String apiKey;
+  static const String apiKey = "QmWTrJJ9W8h3JE19FhCzzPYsJ2tgXZCdUqnbyuo64ToTBN";
+  static const String uri =
+      "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2";
 
-  Uniswap(this.apiKey) {}
+  Uniswap() {}
 
   // Create a graphql client
   Future<GraphQLClient> connect() async {
-    final HttpLink _httpLink = HttpLink(uri: 'https://api.github.com/graphql');
+    final HttpLink _httpLink = HttpLink(uri: uri);
 
     final AuthLink _authLink = AuthLink(
       getToken: () async => 'Bearer $apiKey',
@@ -24,5 +26,30 @@ class Uniswap {
       link: _link,
     );
     return client;
+  }
+
+  static const String pairPrice = r'''
+  query PairPriceAt($adr: String!, $block: Int!) {
+    pair(id: $adr, block: {number: $block}) {
+      id
+      token1Price
+    }
+  }
+''';
+
+  Future<double> pairPriceAt(BlockNum block, EthereumAddress pair) async {
+    try {
+      var options = QueryOptions(
+          documentNode: gql(pairPrice),
+          variables: <String, dynamic>{
+            'adr': pair.hex,
+            'block': block.blockNum
+          });
+      var result = await client.query(options);
+      return double.parse(result.data['pair']['token1Price']);
+    } catch (e) {
+      print("OUCH!");
+      return 0;
+    }
   }
 }
