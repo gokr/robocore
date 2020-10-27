6f35b849abe5557d41a9492df84691d7a94e2d71
Göran Krampe <goran.krampe@gmail.com>
2020-10-27 12:59:39 +0100

    Fixed initialize properly of Pairs

--- lib/model/ethereum.dart
+++ lib/model/ethereum.dart
@@ -25,34 +25,45 @@ class Ethereum {
     // Tokens
     CORE = Token.customAbi(
         client, 'CORE.json', '0x62359Ed7505Efc61FF1D56fEF82158CcaffA23D7');
+    await CORE.initialize();
     WBTC = Token(client, '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599');
+    await WBTC.initialize();
     WETH = Token(client, '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2');
+    await WETH.initialize();
 
     // Uniswap pairs
     CORE2ETH = Pair(
         1, client, "core-eth", '0x32ce7e48debdccbfe0cd037cc89526e4382cb81b');
-    addPair(CORE2ETH);
+    await addPair(CORE2ETH);
     CORE2CBTC = Pair(
         2, client, "core-cbtc", '0x6fad7d44640c5cd0120deec0301e8cf850becb68');
-    addPair(CORE2CBTC);
+    CORE2CBTC.decimals2 = 8;
+    await addPair(CORE2CBTC);
     ETH2USDT = Pair(
         3, client, "eth-usdt", '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852');
-    addPair(ETH2USDT);
+    ETH2USDT.decimals2 = 6;
+    await addPair(ETH2USDT);
     WBTC2USDT = Pair(
         4, client, "wbtc-usdt", '0x0de0fa91b6dbab8c8503aaa2d1dfa91a192cb149');
-    addPair(WBTC2USDT);
+    ETH2USDT.decimals1 = 18;
+    ETH2USDT.decimals2 = 6;
+    await addPair(WBTC2USDT);
     WBTC2ETH = Pair(
         5, client, "wbtc-eth", '0xbb2b8038a1640196fbe3e38816f3e67cba72d940');
-    addPair(WBTC2ETH);
+    WBTC2ETH.decimals1 = 8;
+    await addPair(WBTC2ETH);
 
     // Contracts
     LGE2 = Contract(
         client, 'cLGE.json', '0xf7ca8f55c54cbb6d0965bc6d65c43adc500bc591');
+    await LGE2.initialize();
     COREVAULT = Contract(
         client, 'CoreVault.json', '0xc5cacb708425961594b63ec171f4df27a9c0d8c9');
+    await COREVAULT.initialize();
   }
 
-  addPair(Pair p) {
+  addPair(Pair p) async {
+    await p.initialize();
     pairs[p.name] = p;
   }
 
