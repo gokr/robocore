24fea93e518420f564c5b387a7a7467d11720732
Göran Krampe <goran.krampe@gmail.com>
2020-12-07 20:46:12 +0100

    API added

--- lib/robocore.dart
+++ lib/robocore.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:logging/logging.dart';
@@ -45,8 +46,9 @@ import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:web3dart/web3dart.dart';
 
-import 'package:json_rpc_2/json_rpc_2.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
 
 import 'database.dart';
 
@@ -88,6 +90,9 @@ class Robocore {
 
   /// To interact with Ethereum contracts
   late EthClient ethClient;
+
+  /// API
+  late HttpServer server;
 
   late StreamSubscription subscription;
 
@@ -820,7 +825,7 @@ ${priceStringWBTC()}""";
     });
     */
 
-    await startAPI();
+    startAPI();
 
     // Base background tasks are run every 10 seconds
     final scheduler = NeatPeriodicTaskScheduler(
@@ -839,19 +844,34 @@ ${priceStringWBTC()}""";
   }
 
   startAPI() async {
-    var socket = WebSocketChannel.connect(Uri.parse('ws://localhost:4321'));
+    var app = Router();
 
-    // The socket is a `StreamChannel<dynamic>` because it might emit binary
-    // `List<int>`, but JSON RPC 2 only works with Strings so we assert it only
-    // emits those by casting it.
-    var server = Server(socket.cast<String>());
+    app.get('/hello', (Request request) {
+      return Response.ok('RoboCORE here, hi!');
+    });
 
-    server.registerMethod('tvpl', () {
-      return {
+    app.get('/tvpl', (Request request) async {
+      await updatePriceInfo(null);
+      return Response.ok(jsonEncode({
         'TLLinUSD': TLLinUSD,
         'TVPLinUSD': TVPLinUSD,
-        'floorCOREinUSD': floorCOREinUSD
-      };
+        'floorCOREinUSD': floorCOREinUSD,
+        'floorCOREinETH': floorCOREinETH,
+        'floorLPinUSD': floorLPinUSD,
+        'floorLPinETH': floorLPinETH,
+        'floorLP2inUSD': floorLP2inUSD,
+        'floorLP2inWBTC': floorLP2inWBTC,
+        'floorLP3inUSD': floorLP3inUSD,
+        'floorLP3inFANNY': floorLP3inFANNY,
+        'floorLiquidityETH': floorLiquidityETH,
+        'floorLiquidityWBTC': floorLiquidityWBTC,
+        'floorLiquidityFANNY': floorLiquidityFANNY
+      }));
+    });
+
+    server = await io.serve(app.handler, 'localhost', 10099);
+  }
+
 /*
       floorCOREinUSD,
       floorCOREinETH,
@@ -866,56 +886,6 @@ ${priceStringWBTC()}""";
       floorLiquidityFANNY,
       TLLinUSD,
       TVPLinUSD;*/
-    });
-
-    // Any string may be used as a method name. JSON-RPC 2.0 methods are
-    // case-sensitive.
-    var i = 0;
-    server.registerMethod('count', () {
-      // Just return the value to be sent as a response to the client. This can
-      // be anything JSON-serializable, or a Future that completes to something
-      // JSON-serializable.
-      return i++;
-    });
-
-    // Methods can take parameters. They're presented as a `Parameters` object
-    // which makes it easy to validate that the expected parameters exist.
-    server.registerMethod('echo', (Parameters params) {
-      // If the request doesn't have a "message" parameter this will
-      // automatically send a response notifying the client that the request
-      // was invalid.
-      return params['message'].value;
-    });
-
-    // [Parameters] also supports optional arguments.
-    server.registerMethod('sort', (Parameters params) {
-      var list = params['list'].asList;
-      list.sort();
-      if (params['descendint'].asBoolOr(false)) {
-        return list.reversed;
-      } else {
-        return list;
-      }
-    });
-
-    // A method can send an error response by throwing a `RpcException`.
-    // Any positive number may be used as an application- defined error code.
-    const dividByZero = 1;
-    server.registerMethod('divide', (Parameters params) {
-      var divisor = params['divisor'].asNum;
-      if (divisor == 0) {
-        throw RpcException(dividByZero, 'Cannot divide by zero.');
-      }
-
-      return params['dividend'].asNum / divisor;
-    });
-
-    // To give you time to register all your methods, the server won't start
-    // listening for requests until you call `listen`. Messages are buffered until
-    // listen is called. The returned Future won't complete until the connection
-    // is closed.
-    server.listen();
-  }
 
   logContribution(Contribution c) async {
     await updateLGE3Info();
